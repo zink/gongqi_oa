55a42ce4719b039ead65388aef21b7f2f2a967b5
zink <zinkwind@gmail.com>
2021-02-17 23:35:00 +0800

    dashboard

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,5 +1,25 @@
 <?php
+use \Component\FunctionExtension as Fex;
 class IndexController extends ControllerBase {
     public function indexAction(){
+        $conditions = [];
+        if(!$this->account['is_super']){
+            $conditions = [
+                'conditions'=>'worker_id ='.$this->account['id']
+            ];
+        }
+        $this->view->customer_count = \Customer::count($conditions);
+        $this->view->orders_count = \Orders::count($conditions);
+        if($this->account['is_super']){
+            $this->view->total = \Orders::sum([
+                'column'=>'total'
+            ]);
+        }else{
+            $this->view->total = \Orders::sum([
+                'conditions'=>$conditions->conditions,
+                'column'=>'total'
+            ]);
+        };
+        $this->view->gaap = 0;
     }
 }
